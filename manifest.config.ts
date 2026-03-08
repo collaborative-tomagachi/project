88136f20681e51b2268c24eb5a6e2dd99d004237
Cara Takemoto <cktakemoto@gmail.com>
2026-03-08 14:58:00 -0400

    P-2 remove permission

--- manifest.config.ts
+++ manifest.config.ts
@@ -26,7 +26,7 @@ export default defineManifest({
   background: {
     scripts: ["src/scripts/background/main.ts"],
   },
-  permissions: ["sidePanel", "contentSettings", "notifications", "storage"], // Any Browser APIs we might want to use
+  permissions: ["contentSettings", "notifications", "storage"], // Any Browser APIs we might want to use
   // Sidebar property for firefox isn't supported by @crxjs/vite-plugin
   // But will work, so just ignoring type errors
   // @ts-ignore
